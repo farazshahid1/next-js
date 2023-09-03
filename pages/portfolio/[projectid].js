32785393bc6efd57d2c3b36daece69b8813902a0
m-faraz-shahid <farazshahid123456@gmail.com>
2023-09-03 17:06:36 +0100

    Section 3: Pages & File-based Routing

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,4 +1,12 @@
-function PortfolioProjectPage(params) {
+import { useRouter } from "next/router"
+
+function PortfolioProjectPage() {
+    const router = useRouter();
+
+    console.log(router.pathname)
+    console.log(router.query)
+
+
     return (
         <div>
             <h1>The Portfolio Project Page</h1>
